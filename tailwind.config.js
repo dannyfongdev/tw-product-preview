f0135b46f006fc892cc01e6c79a06edbf0a2cb34
Danny Fong <danny.fong.dev@outlook.com>
2022-12-05 15:16:47 -0800

    Desktop layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Fraunces", "serif"],
+    },
+    colors: {
+      primary: "hsl(158, 36%, 37%)",
+      secondary: "hsl(30, 38%, 92%)",
+      neutral1: "hsl(212, 21%, 14%)",
+      neutral2: "hsl(228, 12%, 48%)",
+      white: "hsl(0, 0%, 100%)"
+    },
     extend: {},
   },
   plugins: [],
-}
+};
